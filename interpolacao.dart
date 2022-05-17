727e6e2883565adb4d2fcfc8ce184571fe083a2b
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-17 17:45:56 -0300

    style: change name of strings

--- interpolacao.dart
+++ interpolacao.dart
@@ -3,10 +3,10 @@ main() {
   String status = 'aprovado';
   double nota = 9.2;
   // basta usar $
-  String usandoConcatenacao = '$nome está $status pq tirou $nota!';
-  print(usandoConcatenacao);
+  String usandoInterpolacao = '$nome está $status pq tirou $nota!';
+  print(usandoInterpolacao);
 
   //basta colocar ${}
-  String concatenacaoComplexa = '${nota.toString()} foi a maior nota da sala!';
-  print(concatenacaoComplexa);
+  String interpolacaoComplexa = '${nota.toString()} foi a maior nota da sala!';
+  print(interpolacaoComplexa);
 }
